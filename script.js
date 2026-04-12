d9a0a65690e21a768328bcb61f3adbdebdc01ed6
ssem0 <ssem@umassd.edu>
2026-04-11 21:25:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function sayHello() {
-    alert("Thanks for visiting my site!");
+// JavaScript = “what it does” (click → popup)
+function sayHello() { // Defines a function named "sayHello"
+    alert("Thanks for visiting my site!"); // Shows a popup message when the function runs
 }
